32320a848d6443062c0b32d7420c2cf24d0a82fc
FrozenString <964413011@qq.com>
2025-11-29 10:37:11 +0800

    ✨添加新的评价指标

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,11 @@
 from sklearn.calibration import label_binarize
 import torch
-from sklearn.metrics import average_precision_score, f1_score, roc_auc_score
+from sklearn.metrics import (
+    average_precision_score,
+    f1_score,
+    roc_auc_score,
+    accuracy_score,
+)
 
 
 def get_link_prediction_metrics(predicts: torch.Tensor, labels: torch.Tensor):
@@ -17,7 +22,8 @@ def get_link_prediction_metrics(predicts: torch.Tensor, labels: torch.Tensor):
     average_precision = average_precision_score(y_true=labels, y_score=predicts)
     roc_auc = roc_auc_score(y_true=labels, y_score=predicts)
 
-    return {'average_precision': average_precision, 'roc_auc': roc_auc}
+    return {"average_precision": average_precision, "roc_auc": roc_auc}
+
 
 def get_link_sign_prediction_metrics(predicts: torch.Tensor, labels: torch.Tensor):
     """
@@ -31,14 +37,24 @@ def get_link_sign_prediction_metrics(predicts: torch.Tensor, labels: torch.Tenso
     labels = labels.cpu().numpy()
 
     y_score = predicts.argmax(1)
-    f1 = f1_score(y_true=labels, y_pred=y_score,zero_division=0,average='macro',)
+    f1 = f1_score(
+        y_true=labels,
+        y_pred=y_score,
+        zero_division=0,
+        average="macro",
+    )
 
-    labels =label_binarize(labels, classes=[0, 1, 2, 3])
-    average_precision = average_precision_score(y_true=labels, y_score=predicts,average='macro')
+    labels = label_binarize(labels, classes=[0, 1, 2, 3])
+    average_precision = average_precision_score(
+        y_true=labels, y_score=predicts, average="macro"
+    )
 
-    return {'average_precision': average_precision, 'f1': f1}
+    return {"average_precision": average_precision, "f1": f1}
 
-def get_link_sign_3class_prediction_metrics(predicts: torch.Tensor, labels: torch.Tensor):
+
+def get_link_sign_3class_prediction_metrics(
+    predicts: torch.Tensor, labels: torch.Tensor
+):
     """
     get metrics for the link prediction task
     :param predicts: Tensor, shape (num_samples, )
@@ -50,12 +66,24 @@ def get_link_sign_3class_prediction_metrics(predicts: torch.Tensor, labels: torc
     labels = labels.cpu().numpy()
 
     y_score = predicts.argmax(1)
-    f1 = f1_score(y_true=labels, y_pred=y_score,zero_division=0,average='macro',)
 
-    labels =label_binarize(labels, classes=[0, 1, 2])
-    average_precision = average_precision_score(y_true=labels, y_score=predicts,average='macro')
+    f1_macro = f1_score(
+        y_true=labels,
+        y_pred=y_score,
+        zero_division=0,
+        average="macro",
+    )
 
-    return {'average_precision': average_precision, 'f1': f1}
+    acc = accuracy_score(labels, y_pred=y_score)
+
+    labels_bin = label_binarize(labels, classes=[0, 1, 2])
+    average_precision = average_precision_score(
+        y_true=labels_bin, y_score=predicts, average="macro"
+    )
+    auc = roc_auc_score(labels_bin, labels=labels, average="macro", multi_class="ovr")
+
+    return {"AP": average_precision, "F1": f1_macro, "acc": acc, "auc": auc}
+
 
 def get_node_classification_metrics(predicts: torch.Tensor, labels: torch.Tensor):
     """
@@ -70,4 +98,4 @@ def get_node_classification_metrics(predicts: torch.Tensor, labels: torch.Tensor
 
     roc_auc = roc_auc_score(y_true=labels, y_score=predicts)
 
-    return {'roc_auc': roc_auc}
+    return {"roc_auc": roc_auc}
